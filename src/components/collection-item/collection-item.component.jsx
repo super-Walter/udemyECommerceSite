3b79522420ad38901ab038afe5e8ae0e3ce4a8ef
Walter Huang <autumnhao@gmail.com>
2021-05-19 16:30:42 -0700

    Add some styles to collection-item to make when user hover on image, then image will change the opacity to 0.8

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './collection-item.styles.scss';
 
@@ -9,6 +10,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <CustomButton>ADD TO CART</CustomButton>
   </div>
 );
 
